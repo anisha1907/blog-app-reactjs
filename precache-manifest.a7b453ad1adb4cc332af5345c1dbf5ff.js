d4247287abc4765e572a7c0865b975fe14723d69
Anisha Sethi <anishasethi24@gmail.com>
2020-07-02 17:15:18 +0530

    Updates

---
+++ precache-manifest.a7b453ad1adb4cc332af5345c1dbf5ff.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "58c40d61e9d84fcea0985f1961feefbb",
+    "url": "/blog-app-reactjs/index.html"
+  },
+  {
+    "revision": "dfd5d25862a34930769f",
+    "url": "/blog-app-reactjs/static/css/main.bf44e7ce.chunk.css"
+  },
+  {
+    "revision": "3717636b22fcdd0c2c43",
+    "url": "/blog-app-reactjs/static/js/2.49ce406d.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/blog-app-reactjs/static/js/2.49ce406d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "dfd5d25862a34930769f",
+    "url": "/blog-app-reactjs/static/js/main.0c0a0d5b.chunk.js"
+  },
+  {
+    "revision": "8b6ba035fabd734ccf6d",
+    "url": "/blog-app-reactjs/static/js/runtime-main.476a2813.js"
+  }
+]);
